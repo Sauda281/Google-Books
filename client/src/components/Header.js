161a42c7c1003e5bb0bb9155029d81b66cc7a8d8
Sauda281 <58537112+Sauda281@users.noreply.github.com>
2020-05-20 12:57:25 -0500

    files

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,5 +1,5 @@
 import React from "react";
-// import image from "../images/books-2.jpg";
+
 
 const headerStyle = {
   "backgroundImage": `url("https://cdn.pixabay.com/photo/2016/01/19/01/42/library-1147815__480.jpg")`,
@@ -12,8 +12,8 @@ const headerStyle = {
 function Header() {
     return (
         <header style={headerStyle}>
-            <h1>(React) Google Books Search</h1>
-            <p>Search for and Save Books of Interest</p>
+            <h1>Google Books React App</h1>
+            <p>Search for your favorite book</p>
         </header>
     );
 }
--- client/src/components/SearchForm.js
+++ client/src/components/SearchForm.js
@@ -7,7 +7,8 @@ function SearchForm(props) {
             <form id="bookSearch">
                 <label htmlFor="bookInput" form="bookSearch">Enter a book to search:</label>
                 <br></br>
-                <input type="text" name="bookInput" id="bookInput" form="bookSearch" onChange={(e) => props.handleChange(e)} placeholder="Book Title" required/>
+                <input type="text" name="bookInput" id="bookInput" form="bookSearch" onChange={(e) => 
+                    props.handleChange(e)} placeholder="Book Title" required/>
                 <br></br>
                 <button type="submit" onClick={(e) => props.handleSearchClick(e)}>Search</button>
             </form>
