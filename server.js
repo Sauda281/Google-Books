a96c31c3944ca9dba3aa0dd8309075eec5a27e50
Sauda281 <58537112+Sauda281@users.noreply.github.com>
2020-05-28 12:59:22 -0500

    Update server.js

--- server.js
+++ server.js
@@ -11,18 +11,6 @@ if (process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"));
 }
 
-
-// const MongoClient = require('mongodb').MongoClient;
-// const uri = "mongodb+srv://Sauda:<Sauda>@cluster0-klqz5.mongodb.net/test?retryWrites=true&w=majority";
-// const client = new MongoClient(uri, { useNewUrlParser: true });
-// client.connect(err => {
-//   const collection = client.db("test").collection("devices");
-//   // perform actions on the collection object
-//   client.close();
-//   console.log ('connected to db')
-// });
-// // 
-
 const mongoose = require("mongoose");
 const mongoURL = process.env.MONGODB_URI|| "mongodb://localhost/google-books"
 mongoose.connect(mongoURL, {useNewUrlParser: true})
